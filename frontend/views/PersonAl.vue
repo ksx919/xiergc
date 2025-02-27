774c8704b619d8e397d7e4aa1e8cbe6f815ae10d
Fuz_zwl <149905780+164321595@users.noreply.github.com>
2025-02-27 14:39:33 +0800

    修改位置

--- frontend/views/PersonAl.vue
+++ frontend/views/PersonAl.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup>
-import PersonalCenter from "../components/PersonalCenter.vue";
+import PersonalCenter from "@/components/PersonalCenter/PersonalCenter.vue";
 </script>
 
 <style scoped>
